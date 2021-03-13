431667c4486480c82ed65bc3ad46bfdc4185fd49
Hirusha-H <77770753+Hirusha-H@users.noreply.github.com>
2021-03-13 16:33:29 +0530

    rt

---
+++ plugins/help_text.py
@@ -0,0 +1,63 @@
+import os
+
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+from script import script
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(filters.command(["help"]))
+def help_user(bot, update):
+
+    bot.send_message(
+        chat_id=update.chat.id,
+        text=script.HELP_USER,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🔰️ Join My Channel 🔰️", url="https://t.me/NexaBotsUpdates")]]),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+@Client.on_message(filters.command(["start"]))
+def send_start(bot, update):
+    
+    bot.send_message(
+        chat_id=update.chat.id,
+        text=script.START_TEXT.format(update.from_user.first_name),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🔰️ Join My Channel 🔰️", url="https://t.me/NexaBotsUpdates")], [InlineKeyboardButton(text="⚜️ Support Group ⚜️", url="https://t.me/Nexa_bots"),
+                                                    InlineKeyboardButton(text="♻️ Share Me ♻️", url="https://t.me/share/url?url=Hi Guys Today I Found Best Telegram Channel Providing <b>Best and Free</b> Bots. Check it Now ❤️ <b>@NexaBotsUpdates</b>")]]),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+
+@Client.on_message(filters.command(["upgrade"]))
+def upgrade(bot, update):
+
+    bot.send_message(
+        chat_id=update.chat.id,
+        text=script.UPGRADE_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+    )
+
+
+@Client.on_message(filters.command(["about"]))
+def about(bot, update):
+    
+    bot.send_message(
+        chat_id=update.chat.id,
+        text=script.ABOUT_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True   
+    ) 
+        
+                
---
+++ script.py
@@ -0,0 +1,83 @@
+class script(object):
+
+    START_TEXT = """Hey <b>{}</b> 😜️ ,
+    
+I'm a simple bot to download **NON-DRM Protected** and **NON-Premium** Videos! 🙂️
+
+Now Send Me Zee5 link I'll upload it to Telegram as File or Video. 😌️
+
+I'm support permenent thumbnail also. 😌️😎️
+
+Smash <b>/help</b> to See Available Commands and Know More About Me 🤗️
+
+Bot by <b>@NexaBotsUpdates</b> . 😊️"""
+
+
+    HELP_USER = """How to use me? 🤔️
+ 
+🌀️ Send Me Custom Thumbnail ( It's permenent )
+
+🌀️ Send Your Zee5 Url and Select Desired Option.
+
+🌀️ Wait till I finish it!
+
+
+<b>Available Commands</b> 🔰️,
+
+ <code>/start</code> - To Start Me 😏️
+ <code>/help</code> - Shows This Help Messege 😅️
+ <code>/setthumb</code> - Set Custom Thumnail 🙂️(Upload or Forward Me a Photo and Reply to it with this command)
+ <code>/showthumb</code> - Show Your Current Custom Thumbnail 😌️
+ <code>/delthumb</code> - To Delete Current Custom Thumbnail 🙃️
+ <code>/upgrade</code> - To Upgrade Your Plan 💸️
+ <code>/about</code> - Show About messege 🧐️
+
+
+Need Support? 🤔️ Visit <b>@Nexa_bots</b>"""
+
+
+    ABOUT_TEXT = """<b>Hey, My Name Is Zee5 Nexa Bot</b>
+
+🌀️<b>Owned By : @NexaBotsUpdates</b>
+
+🌀️<b>Support Group: @Nexa_bots</b>
+
+🌀️<b>Hosted On: Heroku</b>
+
+🌀️<b>Needs Donations? : Yes! Not Rich</b>
+
+<b>Share and Support! ❤️"""
+
+
+
+    FORMAT_SELECTION = """<b>Choose your appropriate option</b> <a href='{}'>⬇️</a>
+
+🎞  - Stream format
+📁  - File format
+
+<b>NOTE ⚠️,
+If you gonna take High Resolution Please Note That Telegram Supports max 2GB Per File Size! 🤕️. Taking High Resolutions Sometimes Make Bot Down! 🤧️. Or Else You'll be Banned! 😌️</b>
+"""    
+    
+    UPGRADE_TEXT = "Please contact my owner in <b>@Nexa_bots</b> or <b>@Bruh_0x</b> 💸️💸️"
+    
+    DOWNLOAD_START = "I'm trying to Download This To My Server! Please wait! 😪️😪️"
+    
+    UPLOAD_START = "Uploading Your File!📤️ Please wait! 😪️😪️"
+    
+    RCHD_TG_API_LIMIT = "Downloaded Your File In {} seconds.\nDetected File Size 🧐️: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations. Sed lyf 🙁️"
+
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "**Thank you for Using Me!! Be Sure To Join @NexaBotsUpdates❤️**"
+    
+    SAVED_CUSTOM_THUMB_NAIL = "<b>Custom thumbnail Saved.✅\nThis thumbnail will be Permanent! 😌️ \n\nTo Delete It Press /delthumb</b>"
+    
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom Thumbnail cleared succesfully.✅"
+    
+    SHOW_THUMB = "**@NexaBotsUpdates**\n\nPress /delthumb to delete this Thumbnail!😪️"
+    
+    NO_THUMB = "Bro Wtf?🙁️  No saved thumbnails 🤕️."
+    
+    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\nMade with ❤️ @NexaBotsUpdates</b>"
+    
+    TIMEOUT = "<b><i>Sorry for the delay 🙂️. It'll help reduce the flood wait</i> 😇\n\nWait for {} sec and try again. 🤗️. by @NexaBotsUpdates</b>"
+    
